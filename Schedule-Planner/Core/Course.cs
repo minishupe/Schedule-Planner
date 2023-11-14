7db012309db14e486ec37eb3d2777a8030cd4b2f
Graham Shupe <grahamshupe04@gmail.com>
2023-11-14 00:57:01 -0800

    Added prefix validator to constructor

--- Schedule-Planner/Core/Course.cs
+++ Schedule-Planner/Core/Course.cs
@@ -14,13 +14,13 @@ namespace Schedule_Planner.Core;
 /// </summary>
 class Course
 {
-    string prefix { get; init; }
-    int code { get; init; }
-    string name { get; init; }
-    int credits { get; init; }
-    Dictionary<int, int> numSections {  get; set; }
+    public string Prefix { get; init; }
+    public int Code { get; init; }
+    public string Name { get; init; }
+    public int Credits { get; init; }
+    public Dictionary<int, int> NumSections {  get; set; }
     private static readonly string dataPath = $@"{Environment.ProcessPath}\Data\";
-    private const string timetableUrl = "https://web4u.banner.wwu.edu/pls/wwis/wwskcfnd.TimeTable"
+    private const string TimetableUrl = "https://web4u.banner.wwu.edu/pls/wwis/wwskcfnd.TimeTable";
 
 
     /// <summary>
@@ -36,20 +36,54 @@ class Course
     /// </summary>
     public Course(string prefix, int code, int term, IWebDriver driver)
     {
-        if (driver.Url != timetableUrl)
+        if (driver.Url != TimetableUrl)
         {
-            driver.Url = timetableUrl;
+            driver.Url = TimetableUrl;
         }
         if (driver.Title != "WWU TimeTable of Classes")
         {
             Log.Warning($"Unexpected TimeTable title: {driver.Title}");
         }
-        var subjSelector = new SelectElement(driver.FindElement(By.Id("subj")));
 
+        // Check the prefix exists:
+        var subjSelector = new SelectElement(driver.FindElement(By.Id("subj")));
+        IList<IWebElement> subjects = subjSelector.Options;
+        foreach (IWebElement item in subjects)
+        {
+            if (item.GetAttribute("value") == prefix)
+            {
+                this.Prefix = prefix;
+                break;
+            }
+        }
+        if (Prefix == null)
+        {
+            Log.Warning($"Could not find prefix {prefix} in subject list");
+            throw new ArgumentException($"Course prefix {prefix} is not valid");
+        }
+
+        // Check the term exists:
+
+        
     }
 
     public AddTerm(int term, IWebDriver driver)
     {
 
+    }
+
+    /// <summary>
+    /// Checks whether a given value exists in a list of web elements.
+    /// </summary>
+    private bool CheckExists(string item, IList<IWebElement> options)
+    {
+        foreach (IWebElement option in options)
+        {
+            if (option.GetAttribute("value") == item)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
