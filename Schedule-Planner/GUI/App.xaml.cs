ada7f49bc21cca2ec400b4b20648587f4b205528
Graham Shupe <grahamshupe04@gmail.com>
2024-01-11 15:36:08 -0800

    Updated namespaces

--- Schedule-Planner/GUI/App.xaml.cs
+++ Schedule-Planner/GUI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Schedule_Planner
+namespace Schedule_Planner.GUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Schedule-Planner/GUI/MainWindow.xaml.cs
+++ Schedule-Planner/GUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Schedule_Planner
+namespace Schedule_Planner.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
