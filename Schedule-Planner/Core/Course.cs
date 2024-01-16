f400a17113b6fe6b77423add13cf960aa858af37
Graham Shupe <grahamshupe04@gmail.com>
2024-01-15 16:45:39 -0800

    AddTerm() completed

--- Schedule-Planner/Core/Course.cs
+++ Schedule-Planner/Core/Course.cs
@@ -16,9 +16,9 @@ class Course
 {
     public string Prefix { get; }
     public int Code { get; }
-    public string Name { get; }
-    public int Credits { get; }
-    public Dictionary<int, List<ISection>> Sections { get; private set; } // keys are terms, values are associated classes
+    public string? Name { get; private set; }
+    public int Credits { get; private set; }
+    public Dictionary<int, List<Lecture>> Sections { get; private set; } // keys are terms, values are associated classes
     private static readonly string dataPath = $@"{Environment.ProcessPath}\Data\";
     private const string TimetableUrl = "https://web4u.banner.wwu.edu/pls/wwis/wwskcfnd.TimeTable";
 
@@ -62,67 +62,29 @@ class Course
             Log.Warning($"Given prefix does not exist: {prefix}");
             throw new ArgumentException($"Given prefix does not exist: {prefix}");
         }
+        this.Prefix = prefix;
+        this.Code = code;
 
         // Call CreateSections() to validate term and set the actual sections.
         bool addSuccess = AddTerm(term, driver);
 
-
-
-
-        /*
-        // Check to make sure term and prefix exist:
-        // TODO: save terms and subjects locally, only search through selectlist if it doesn't exist locally.
-        string? invalidArgs = CheckInvalid(prefix, term.ToString(), driver);
-        if (invalidArgs != null)
+        if (!addSuccess)
         {
-            Log.Warning($"Given prefix or term does not exist: {invalidArgs}");
-            throw new ArgumentException($"Given prefix or term does not exist: {invalidArgs}");
+            Log.Warning($"Given term {term} or course code {code} does not exist");
+            throw new ArgumentException($"Given term {term} or course code {code} does not exist");
         }
-
-        // Navigate through timetable and scrape course data:
-        var submitButton = driver.FindElement(By.XPath("//input[@value='Submit'"));
-        submitButton.Click();
-
-        Sections = new Dictionary<int, List<ISection>>
-        {
-            { term, new List<ISection>() }
-        };
-
-        var courseTitles = driver.FindElements(By.XPath("//td[@class='fieldformatboldtext']"));
-        foreach (IWebElement course in courseTitles)
-        {
-            string[] splitText = course.Text.Split(" ");
-            if (splitText[0] == prefix && splitText[1] == code.ToString())
-            {
-                Credits = (splitText[^1])[0];
-                var courseSections = driver.FindElements(RelativeBy.WithLocator(By.XPath("//table")).Below(course));
-
-                foreach (IWebElement section in courseSections)
-                {
-                    var sectionInfo = section.FindElements(By.XPath(".//td"));
-                    // skips header and other random table elements:
-                    if (sectionInfo.Count() == 0 || sectionInfo[0].Text == "Term")
-                    {
-                        continue;
-                    }
-                    // signifies a lab section:
-                    else if (sectionInfo[0].Text == "")
-                    {
-                        Sections.GetValueOrDefault(term).Add(new Lab());
-                    }
-                }
-            }
-        }
-        */
     }
 
 
     /// <summary>
     /// Adds a term and its corresponding sections to this object's Sections.
+    /// <para />
+    /// <br>Preconditions: Subject prefix exists within Timetable. </br>
     /// </summary>
     /// <returns>A boolean denoting whether the term and its sections were successfully added.</returns>
-    public bool AddTerm(int term, IWebDriver driver)
+    public bool AddTerm(int term, IWebDriver driver, bool updateInfo = false)
     {
+        bool success = false;
         // Load onto timetable and get the subject, if needed:
         if (driver.Url != TimetableUrl)
         {
@@ -154,71 +116,61 @@ class Course
         // Create new section list for the term, creating a new term if necessary:
         if (Sections == null)
         {
-            Sections = new Dictionary<int, List<ISection>> { { term, new List<ISection>() } };
+            Sections = new Dictionary<int, List<Lecture>> { { term, new List<Lecture>() } };
         }
         else
         {
-            Sections[term] = new List<ISection>();
+            Sections[term] = new List<Lecture>();
         }
 
-        return CreateSections(term, driver);
-    }
-
-    /// <summary>
-    /// Adds a list of sections to the given term
-    /// Precondition: driver is loaded onto the Timetable with the desired term and subject,
-    /// the Sections, Code, and Prefix fields have been initialized.
-    /// </summary>
-    private bool CreateSections(int term, IWebDriver driver)
-    {
-        
-    }
-
-
-    /// <summary>
-    /// Checks whether a given subject and term exist in the timetable.
-    /// Returns: a string denoting the invalid prefix or term, or null if both are valid.
-    /// </summary>
-    /*
-    private string? CheckInvalid(string prefix, string term, IWebDriver driver)
-    {
-        var subjSelector = new SelectElement(driver.FindElement(By.Id("subj")));
-        IList<IWebElement> subjects = subjSelector.Options;
-        var termSelector = new SelectElement(driver.FindElement(By.Id("term")));
-        IList<IWebElement> terms = termSelector.Options;
-        bool subjValid = false;
-        int i = 0;
-
-        // subjects = 5, terms = 3, i[end] = 8
-        // 
-
-        while (i < subjects.Count + terms.Count) 
+        // Add sections to the term:
+        var courseTitles = driver.FindElements(By.XPath("//td[@class='fieldformatboldtext']"));
+        foreach (IWebElement course in courseTitles)
         {
-            if (i < subjects.Count)
+            string[] splitText = course.Text.Split(" ");
+            if (splitText[0] != Prefix && splitText[1] != Code.ToString())
             {
-                if (subjects[i].GetAttribute("value") == prefix) 
-                { 
-                    // subject exists, so skip to checking term.
-                    subjValid = true;
-                    i = subjects.Count;
-                }
+                continue;
             }
-            else
+
+            if (updateInfo) // update name and credits.
             {
-                if (terms[i - subjects.Count].GetAttribute("value") == term)
+                Credits = splitText[^1][0]; // Possible error? Can you implicitly convert char to int?
+                Name = course.FindElement(By.TagName("a")).Text;
+            }
+            
+            var courseSections = driver.FindElements(RelativeBy.WithLocator(By.XPath("//table")).Below(course));
+
+            foreach (IWebElement section in courseSections)
+            {
+                var sectionInfo = section.FindElements(By.XPath(".//td"));
+                // skips header and other random table elements:
+                if (sectionInfo.Count() == 0 || sectionInfo[0].Text == "Term")
                 {
-                    // term exists and subject exists, so return.
-                    return null;
+                    continue;
+                }
+                // signifies a lab section:
+                else if (sectionInfo[0].Text == "")
+                {
+                    Sections[term].Last().Lab = new Lab(section);
+
+                }
+                // It is a course title:
+                else if (sectionInfo.Count < 13) 
+                {
+                    break;
+                }
+                // should be a normal lecture section now:
+                else
+                {
+                    Sections[term].Add(new Lecture(section));
+                    success = true;
                 }
             }
-            if (i == subjects.Count - 1 && subjValid == false)
-            {
-                // subject does not exist.
-                return prefix;
-            }
+            break;
         }
-        // term does not exist.
-        return term;
+        return success;
     }
-    */
+
+
 }
