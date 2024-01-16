165a10b8f022e44792348127e94e1280aece9dbf
Graham Shupe <grahamshupe04@gmail.com>
2024-01-16 01:26:16 -0800

    New class

---
+++ Schedule-Planner/Core/Instructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule_Planner.Core;
+
+/// <summary>
+/// Represents a professor, which can be added to any section.
+/// </summary>
+public class Instructor
+{
+    public string Name { get; }
+    public string Id { get; set; }
+    public float Rating { get; private set; }
+    public float Difficulty { get; private set; }
+
+    public Instructor()
+    {
+
+    }
+
+    /// <summary>
+    /// Fetches instructor data from Rate My Professors and converts it into an
+    /// Instructor object.
+    /// </summary>
+    /// <returns>An Instructor instance</returns>
+    private static Instructor Fetch()
+    {
+        // I should do this for courses too.
+
+    }
+}
